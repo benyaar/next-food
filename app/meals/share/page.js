be9cf91a3ce99b7ca9cc7ae60d7759dd8667a2a7
Artur <82472015+benyaar@users.noreply.github.com>
2025-05-16 14:44:40 +0300

    add form submit button

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,9 @@
+import MealsFormSubmit from '@/components/meals/form-submit/meals-form-submit';
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker/image-picker";
 import {shareMeal} from "@/lib/action";
 
-export default function ShareMealPage() {
+export default function ShareMealPage() { 
     return (
         <>
             <header className={classes.header}>
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label="Your name" name="image" />
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit/>
                     </p>
                 </form>
             </main>
